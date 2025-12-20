d9365cb43d2795808aad2fce193c093c9367d5ad
yograj06 <yograj240607@gmail.com>
2025-12-20 12:14:26 +0530

    added text and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./components/Login"
-import StudentDashboard from "./components/StudentDashboard"
-import TeacherDashboard from "./components/TeacherDashboard"
-import AdminDashboard from "./components/AdminDashboard"
-import ProtectedRoute from "./components/ProtectedRoute"
-import StudentLayout from "./components/StudentLayout"
-import TeacherLayout from "./components/TeacherLayout"
-import AdminLayout from "./components/AdminLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import StudentDashboard from "./components/StudentDashboard";
+import TeacherDashboard from "./components/TeacherDashboard";
+import AdminDashboard from "./components/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import StudentLayout from "./components/StudentLayout";
+import TeacherLayout from "./components/TeacherLayout";
+import AdminLayout from "./components/AdminLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* LOGIN PAGE */}
+        {/* Login Page */}
         <Route
           path="/"
           element={
             <div className="min-h-screen flex flex-col bg-gray-200 px-4">
+              {/* Center Content */}
               <div className="grow grid place-items-center">
-                <Login />
+                <div className="flex flex-col items-center gap-4">
+                  <h1 className="text-4xl font-bold mt-6 text-center">
+                    Welcome
+                  </h1>
+
+                  <h2 className="text-2xl text-gray-700 font-semibold text-center max-w-md mb-6">
+                    Face Recognition Attendance System
+                  </h2>
+
+                  <Login />
+                </div>
               </div>
+
+              {/* Footer */}
+              <footer className="text-center mt-6 mb-5 sm:mt-10 text-gray-800 text-xs sm:text-sm border-t border-gray-500 pt-4">
+                © 2025 | Attendance App — All rights reserved • Made with ❤️ by
+                Yograj
+              </footer>
             </div>
           }
         />
@@ -60,10 +76,9 @@ const App = () => {
         >
           <Route index element={<AdminDashboard />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
